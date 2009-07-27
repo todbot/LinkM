e22d119ae428b8d8bf7306db642db01bd1ced4dd
Tod E. Kurt <todbot@users.noreply.github.com>
2009-07-27 22:31:16 +0000

    fixed includes for new dir layout, added jar check for linkm.sh

--- java_host/linkm.sh
+++ java_host/linkm.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+
+# if you haven't made the jar:
 #java thingm.linkm.LinkM $*
-java -jar linkm.jar $*
+
+# but normally
+if [ -e linkm.jar ]; then
+    java -jar linkm.jar $*
+else 
+    echo "cannot run. make the jar with 'make jar' please"
+fi
