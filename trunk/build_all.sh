f0eacdaf3da7d01148fd07ac5211c2c1aca85b8e
tod.kurt@gmail.com <tod.kurt@gmail.com@975a1b08-7af9-11de-a1b3-85d0aeae5c5b>
2010-04-22 20:13:25 +0000

    added build_all

---
+++ trunk/build_all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# FIXME: doesn't work for all OS types
+
+pushd bootloadHID/commandline
+make clean && make
+popd
+
+pushd c_host
+make clean && make
+popd
+
+pushd java_host
+make clean && make jar && make processing
+popd
