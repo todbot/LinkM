647a6ee6201e77263e96fddd5b5d22d3649e4aaa
tod.kurt@gmail.com <tod.kurt@gmail.com@975a1b08-7af9-11de-a1b3-85d0aeae5c5b>
2010-06-17 02:02:45 +0000

    put back x86_arch for macosx

--- trunk/java_host/linkm.sh
+++ trunk/java_host/linkm.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
+#
+# you must type "make jar" before this script will work
+#
 
-# if you haven't made the jar:
-#java thingm.linkm.LinkM $*
-
-# but normally
-if [ -e linkm.jar ]; then
-#    java -d32 -jar linkm.jar $*
-    java -Djava.library.path=. -jar linkm.jar $*
+if [ -e libtargets/linkm.jar ]; then
+#    java -d32 -Djava.library.path=libtargets -jar libtargets/linkm.jar $*
+    java -d32 -Djava.library.path=libtargets -jar libtargets/linkm.jar $*
 else 
     echo "cannot run. make the jar with 'make jar' please"
 fi
