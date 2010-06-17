7b6055a37f6722d8e797bf660ac43bbc8eb4781e
tod.kurt@gmail.com <tod.kurt@gmail.com@975a1b08-7af9-11de-a1b3-85d0aeae5c5b>
2010-06-17 04:32:59 +0000

    added windows batch file version of linkm.sh

--- trunk/java_host/linkm.bat
+++ trunk/java_host/linkm.bat
@@ -1,6 +1,6 @@
-#
-# you must type "make jar" before this script will work
-#
+REM
+REM you must type "make jar" before this script will work
+REM
 @echo off
 java -Djava.library.path=libtargets -jar libtargets/linkm.jar %1 %2 %3 %4 %5
 
